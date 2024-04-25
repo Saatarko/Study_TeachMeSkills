f8bd41350fe128c700ee0f5ff3860f05095edacf
Saatarko <saatarko@tut.by>
2024-04-25 14:42:29 +0300

    Random task1.0,95

--- Random_Task/Task_Class(HumanByHouse).py
+++ Random_Task/Task_Class(HumanByHouse).py
@@ -1,7 +1,7 @@
 from tkinter import *  # Добавляем библиотеку Ткинтер
 from tkinter import ttk  # Добавляем модуль Ткинтера
 from My_Function import *  # Добавляем библиотеку Function
-from importlib import import_module
+import csv
 
 human_by_house_menu = Tk()
 
@@ -24,6 +24,8 @@ human_by_house_menu["bg"] = "gray22"  # gray gray0-99   # Устанавлива
 
 new_human = []  # переменная для хранения человеков
 new_house = []  # переменная для хранения домов
+human_name = []  # переменная для хранения имен (для выпадающего списка)
+house_name = []  # переменная для хранения имен домов (для выпадающего списка)
 
 
 class MyException(Exception):
@@ -82,35 +84,34 @@ class Human:
 
     def earn_money(self, money):
         self.money += money
-        print(f'Заработано: {money}. Итого в наличии {self.money}')
 
     def buy_house(self, house, discount):
         price = house.final_price(discount)
-        if self.money < price:
-            print(f'К сожалению денег на дом не хватает. Нужно еще {price - self.money}')
-        else:
+        if self.money > price:
             self.__make_deal(house, price)
+        else:
+            mb.showwarning(f"Внимание,", f'Денег не хватает. Надо еще заработать {price - self.money}')
 
 
 class House:
 
-    def __init__(self, area, price):
-        self._area = area
-        self._price = price
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+        # Метод для регулировки вывода данных в виде текста
+
+    def __str__(self):
+        return f'Название: {self.name}, Цена: {self.price}'
+
+    # Метод для регулировки вывода данных в виде кода
+    def __repr__(self):
+        return f'(\'{self.name}\', {self.price})'
 
     def final_price(self, discount):
-        final_price = self._price * (100 - discount) / 100
-        print(f'Цена за дом: {self._price} со скидкой {discount} составляет {final_price}')
+        final_price = self.price * (100 - discount) / 100
+
         return final_price
-
-
-class SmallHouse(House):
-    default_area = 40
-
-    def __init__(self, price):
-        super().__init__(SmallHouse.default_area, price)
-
-        self._price = price
 
 
 def get_enter():  # Создаем подкласс (второе окно)
@@ -127,7 +128,11 @@ def get_enter():  # Создаем подкласс (второе окно)
 
         text_class.config(state="normal")
         text_class.delete('1.0', END)
+        human_name.clear()
         for human in new_human:
+            human_name.append(human.name)
+            combobox_by_house['values'] = human_name
+            combobox_by_house3['values'] = human_name
             text_class.insert(END, f'{human}\n')
         text_class.config(state="disabled")
 
@@ -145,10 +150,12 @@ def get_enter():  # Создаем подкласс (второе окно)
                 age, check = value_check_func(age)
                 if check is False:
                     raise ExceptionStrInNumber("Вместо цифры в поле -строка")  # Генерация ошибки если поле строка
-
+                elif age <= 0:
+                    raise ZeroDivisionError("Невозможное значение для этого поля")
             except ExceptionStrInNumber:
                 mb.showwarning(f"Ошибка,", 'Вы ввели не число')
-
+            except ZeroDivisionError:
+                mb.showwarning(f"Ошибка,", 'Проверьте значение возраста')
             else:
 
                 new_hum = Human(name, age)
@@ -157,19 +164,13 @@ def get_enter():  # Создаем подкласс (второе окно)
                 entry_by_house.delete(0, END)
                 entry_by_house2.delete(0, END)
                 on_screen_human()
-                d = repr(new_human)
-                for i in new_human:         # Прогонка по списку в цикле с обращениемк к переменной как к полям класса
-                                                # код верменный всунутьв кнопку
-                    if i.name == 'Alex':
-                        i.earn_money(5000)
-                a = 0
 
     def add_home():
 
         try:
-            house_name = entry_by_house3.get()
+            temp_house_name = entry_by_house3.get()
             price = entry_by_house4.get()
-            if not house_name or not price:
+            if not temp_house_name or not price:
                 raise ExceptionNullField("Одно из полей пустое")  # Генерация ошибки если поле пустое
         except ExceptionNullField:
             mb.showwarning(f"Ошибка,", 'Не все данные заполнены!')
@@ -178,16 +179,105 @@ def get_enter():  # Создаем подкласс (второе окно)
                 price, check = value_check_func(price)
                 if check is False:
                     raise ExceptionStrInNumber("Вместо цифры в поле -строка")  # Генерация ошибки если поле строка
-
+                elif price <= 0:
+                    raise ZeroDivisionError("Невозможное значение для этого поля")
             except ExceptionStrInNumber:
                 mb.showwarning(f"Ошибка,", 'Вы ввели не число')
+            except ZeroDivisionError:
+                mb.showwarning(f"Ошибка,", 'Бесплатные дома закончились>')
 
             else:
-                new_house.append(House(house_name, price))
-                text_class.config(state="normal")
-                text_class.delete('1.0', END)
-                text_class.insert(END, f'{new_house}')
-                text_class.config(state="disabled")
+
+                new_house_temp = House(temp_house_name, price)
+                new_house.append(new_house_temp)
+
+                entry_by_house3.delete(0, END)
+                entry_by_house4.delete(0, END)
+
+                house_name.clear()
+                for house in new_house:
+                    house_name.append(house.name)
+                    combobox_by_house2['values'] = house_name
+
+    def add_earn_money():
+
+        try:
+            temp_str = combobox_by_house.get()
+            temp_salary = entry_by_house5.get()
+            if not temp_str or not temp_salary:
+                raise ExceptionNullField("Одно из полей пустое")  # Генерация ошибки если поле пустое
+        except ExceptionNullField:
+            mb.showwarning(f"Ошибка,", 'Не все данные заполнены!')
+        else:
+            try:
+                temp_salary, check = value_check_func(temp_salary)
+                if check is False:
+                    raise ExceptionStrInNumber("Вместо цифры в поле -строка")  # Генерация ошибки если поле строка
+                elif temp_salary <= 0:
+                    raise ZeroDivisionError("Невозможное значение для этого поля")
+            except ExceptionStrInNumber:
+                mb.showwarning(f"Ошибка,", 'Вы ввели не число')
+            except ZeroDivisionError:
+                mb.showwarning(f"Ошибка,", 'Смысл начислять зп 0?')
+
+            for i in new_human:  # Прогонка по списку в цикле с обращением к переменной как к полям класса
+                if i.name == temp_str:
+                    i.earn_money(temp_salary)
+            on_screen_human()
+
+    def add_by_house():
+
+        try:
+            temp_discount = entry_by_house6.get()
+            temp_house = combobox_by_house2.get()
+            temp_human = combobox_by_house3.get()
+            if not temp_house or not temp_human:
+                raise ExceptionNullField("Одно из полей пустое")  # Генерация ошибки если поле пустое
+        except ExceptionNullField:
+            mb.showwarning(f"Ошибка,", 'Не все данные заполнены!')
+        else:
+            try:
+                if temp_discount == '':
+                    temp_discount = 0  # Скидка по умолчанию
+
+                temp_discount, check = value_check_func(temp_discount)
+
+                if check is False:
+                    raise ExceptionStrInNumber("Вместо цифры в поле -строка")  # Генерация ошибки если поле строка
+                elif temp_discount < 0:
+                    raise ZeroDivisionError("Невозможное значение для этого поля")
+            except ExceptionStrInNumber:
+                mb.showwarning(f"Ошибка,", 'Вы ввели не число')
+            except ZeroDivisionError:
+                mb.showwarning(f"Ошибка,", 'Скидка не может быть меньше 0')
+
+            for i in new_human:  # Прогонка по списку в цикле с обращением к переменной как к полям класса
+                if i.name == temp_human:
+                    for j in new_house:
+                        if j.name == temp_house:
+                            i.buy_house(j, temp_discount)
+            on_screen_human()
+
+    def on_save():
+
+        with open('list_human.csv', 'w', newline='') as f:
+            csv.writer(f).writerows(Human)
+        with open('list_house.csv', 'w', newline='') as f:
+            csv.writer(f).writerows(Hiuse)
+
+    def on_load():
+
+        with open('list_human.csv', 'r', newline='') as f:
+            csvreader = csv.reader(f)
+            fields = next(csvreader)
+            for row in csvreader:
+                new_human.append(row)
+
+        with open('list_house.csv', 'r', newline='') as f:
+            csvreader = csv.reader(f)
+            fields = next(csvreader)
+            for row in csvreader:
+                new_house.append(row)
 
     def on_close():  # Кнопка закрытия на крестик
 
@@ -247,19 +337,46 @@ def get_enter():  # Создаем подкласс (второе окно)
                           borderwidth=2, relief="solid")
     lab_by_house9.place(x=25, y=280, width=250, height=25)
 
-    lab_by_house10 = Label(human_by_house, text='Выберите человека и дом для покупки', font='Arial 9 bold',
+    lab_by_house10 = Label(human_by_house, text='Выберите дом и человека для покупки', font='Arial 9 bold',
                            borderwidth=2, relief="solid")
     lab_by_house10.place(x=315, y=280, width=250, height=25)
 
-    # combobox_tab3 = ttk.Combobox(human_by_house, values = Human.name, state="readonly")
-    # combobox_tab3.place(x=10, y=120, width=480, height=30)
+    combobox_by_house = ttk.Combobox(human_by_house, values=human_name, state="readonly")
+    combobox_by_house.place(x=10, y=315, width=135, height=30)
 
-    #
-    # button_lab_imt = Button(lesson_8, text='Рассчитать', font='Arial 12 bold ', command=calculate_imt, borderwidth=2)
-    # button_lab_imt.place(x=10, y=165, width=580, height=30)
-    #
+    entry_by_house5 = Entry(human_by_house, font='Arial 10 bold', width=15, borderwidth=2)
+    entry_by_house5.place(x=155, y=315, width=135, height=30)
+
+    button_by_house3 = Button(human_by_house, text='Начислить', font='Arial 12 bold ',
+                              command=add_earn_money, borderwidth=2)
+    button_by_house3.place(x=10, y=355, width=280, height=30)
+
+    combobox_by_house2 = ttk.Combobox(human_by_house, values=house_name, state="readonly")
+    combobox_by_house2.place(x=315, y=315, width=120, height=30)
+
+    combobox_by_house3 = ttk.Combobox(human_by_house, values=human_name, state="readonly")
+    combobox_by_house3.place(x=445, y=315, width=120, height=30)
+
+    lab_by_house11 = Label(human_by_house, text='Скидка', font='Arial 9 bold')
+    lab_by_house11.place(x=315, y=355, width=120, height=30)
+
+    entry_by_house6 = Entry(human_by_house, font='Arial 10 bold', width=15, borderwidth=2)
+    entry_by_house6.place(x=445, y=355, width=120, height=30)
+
+    button_by_house3 = Button(human_by_house, text='Купить дом', font='Arial 12 bold ',
+                              command=add_by_house, borderwidth=2)
+    button_by_house3.place(x=315, y=395, width=250, height=30)
+
     text_class = Text(human_by_house, font='Arial 10', width=15, borderwidth=2, wrap="word", state="disabled")
     text_class.place(x=10, y=690, width=580, height=100)
+
+    button_save = Button(human_by_house, text='Сохранить данные в файл', font='Arial 12 bold ',
+                         command=on_save, borderwidth=2)
+    button_save.place(x=10, y=610, width=580, height=30)
+
+    button_load = Button(human_by_house, text='Загрузить данные из файла', font='Arial 12 bold ',
+                         command=on_load, borderwidth=2)
+    button_load.place(x=10, y=650, width=580, height=30)
 
     button_exit = Button(human_by_house, text='Выход', font='Arial 12 bold ', command=on_close, borderwidth=2)
     button_exit.place(x=10, y=800, width=580, height=30)
@@ -293,19 +410,3 @@ human_by_house_menu.protocol('WM_DELETE_WINDOW', get_exit)
 human_by_house_menu.mainloop()
 
 # endregion
-
-#
-# if __name__ == '__main__':
-#     # Human.defaultinfo()
-#     Deniska = Human('Дениска', 42)
-#     Deniska.info()
-#     Deniska.earn_money(5000)
-#     # Human.defaultinfo()
-#     # a = 0
-#     #
-#     lof_house = SmallHouse(15000)
-#     Deniska.buy_house(lof_house, 10)
-#     #
-#     Deniska.earn_money(20000)
-#     Deniska.buy_house(lof_house, 10)
-#     Deniska.info()
