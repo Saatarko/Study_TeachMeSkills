7dd563c485090e5080ea14b8e1b1f348130cefc8
saatarko <saatar@tut.by>
2024-05-05 16:33:28 +0300

    fix data verification

--- Telebot/Telebot.py
+++ Telebot/Telebot.py
@@ -1,3 +1,5 @@
+import re
+
 import telebot  # Импортируем библиотеку для телеботов
 from telebot import types  # Импортируем метод types (для кнопок)
 from telebot.types import ReplyKeyboardRemove  # Импортируем метод для удаления кнопок
@@ -47,14 +49,25 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, text='Спасибо за пользование программой', reply_markup=ReplyKeyboardRemove())
 
         bot.send_message(chat_id=-4266542112, text=f'Был  сформирован запрос от '
-                                                     f'{new_human[-1].name} на машину {new_human[-1].car} с '
-                                                     f'максимальной суммой {new_human[-1].money}')
+                                                   f'{new_human[-1].name} на машину {new_human[-1].car} с '
+                                                   f'максимальной суммой {new_human[-1].money}')
 
 
 def process_name_step(message):  # Функция пошагового запроса для получения данных (ФИО)
     try:
 
         name = message.text
+
+        if not isinstance(name, str):  # Проверка на то что бы ФИо были строкой
+
+            message = bot.reply_to(message, 'Имя и Фамилия из цифр -  что-то новое!')
+            bot.register_next_step_handler(message, process_name_step)
+            return
+        elif not re.search(r'[а-яё]{2,}\s[а-яё]{2,}', name.lower()):
+            message = bot.reply_to(message, 'То что вы написали, не очень похоже на Имя и Фамилию')
+            bot.register_next_step_handler(message, process_name_step)
+            return
+
         user = Human(name)  # Создаем элемент класса
         new_human.append(user)  # Загоняем эк в список
         message = bot.reply_to(message, 'Машину какой марки Вы бы хотели приобрести? (Mersedes, Volvо и т.д.)')
@@ -67,6 +80,17 @@ def process_car_step(message):  # Функция пошагового запро
     try:
 
         car = message.text
+
+        if not isinstance(car, str):  # Проверка на то что бы ФИо были строкой
+
+            message = bot.reply_to(message, 'Марка машины из цифр -  что-то новое!')
+            bot.register_next_step_handler(message, process_car_step)
+            return
+        elif not re.search(r'[а-яё]{2,}(\s[а-яё]{2,})?', car.lower()):
+            message = bot.reply_to(message, 'То что вы написали, не очень похоже на марку машины')
+            bot.register_next_step_handler(message, process_car_step)
+            return
+
         new_human[-1].car = car  # Экземпляр класса уже есть и в списке. Присваиваем ему новое авто
         message = bot.reply_to(message, 'Сколько у Вас денег для покупки?')
         bot.register_next_step_handler(message, process_money_step)  # Пошаговый запрос
@@ -78,10 +102,12 @@ def process_money_step(message):  # Функция пошагового запр
     try:
 
         money = message.text
-        if not money.isdigit():  # Проверка на то что бы денежки были цифрой
-            message = bot.reply_to(message, 'Значение денег - это цифра')
+        if not money.isdigit() or int(money) < 0:  # Проверка на то что бы денежки были цифрой
+
+            message = bot.reply_to(message, 'Значение денег - это положительная цифра')
             bot.register_next_step_handler(message, process_money_step)
             return
+
         new_human[-1].money = money  # Экземпляр класса уже есть и в списке. Присваиваем ему новое значение денег
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создаем кнопки с запросом корректности данных
