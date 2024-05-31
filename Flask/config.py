0f720012561296a3f6fc4ff34850a8599883f56d
saatarko <saatar@tut.by>
2024-05-31 22:57:40 +0300

    Flask. all-in -one

--- Flask/config.py
+++ Flask/config.py
@@ -1,25 +1,25 @@
 import os
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+#
+#
+# class Settings(BaseSettings):
+#     DB_HOST: str
+#     DB_PORT: int
+#     DB_USER: str
+#     DB_PASS: str
+#     DB_NAME: str
+#
+#     @property
+#     def database_url_psycopg(self):
+#         # DSN
+#         # postgresql+psycopg://postgres:postgres@localhost:5432/sa
+#         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+#
+#     model_config = SettingsConfigDict(env_file=".env")
 
-
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def database_url_psycopg(self):
-        # DSN
-        # postgresql+psycopg://postgres:postgres@localhost:5432/sa
-        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file=".env")
-
-
-settings = Settings()
+#
+# settings = Settings()
 
 
 class Config:
